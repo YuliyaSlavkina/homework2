54b15688a56fe08231f541acbb523357596693b9
Julia <wuwara@mail.ru>
2022-12-04 14:38:20 +0300

    решение двух задач

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// int SecondNumber() {
+//     Console.WriteLine("Введите трехзначное число: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     int dec = num / 10;
+//     return dec % 10; 
+// }
+
+// int result = SecondNumber();
+// Console.WriteLine (result);
+
+
+// void Weekend(int num){
+    
+//     if (num >0 && num < 5)
+//         Console.WriteLine("Это не выходной день");
+//     else if (num > 5 && num < 8)
+//         Console.WriteLine("Ура! Это выходной день!");
+//     else
+//         Console.WriteLine("Введите число от 1 до 7");
+// }
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Weekend(num);
