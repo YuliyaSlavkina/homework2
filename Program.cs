057535d9b7c89c015445614576f476a136c11aac
Julia <wuwara@mail.ru>
2022-12-06 22:32:06 +0300

    подкорректировала третью задачу

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
-// int SecondNumber() {
-//     Console.WriteLine("Введите трехзначное число: ");
-//     int num = Convert.ToInt32(Console.ReadLine());
-//     int second_number = num / 10;
-//     return second_number % 10; 
-// }
+int SecondNumber() {
+    Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int second_number = num / 10;
+    return second_number % 10; 
+}
 
-// int result = SecondNumber();
-// Console.WriteLine (result);
+int result = SecondNumber();
+Console.WriteLine (result);
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
@@ -29,18 +29,18 @@
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-void ThirdNumber (int num) {
-    if (num > 100 && num < 1000){
-        int third = num % 10;
-        Console.WriteLine(third);
-    }
-    else if (num < 100)
-        Console.WriteLine("Третьей цифры нет.");
-    else 
-        while (num > 1000)
-        num = num / 10;
-        Console.WriteLine(num % 10);
-}
- Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
- ThirdNumber (num);
+// void ThirdNumber (int num) {
+//     if (num > 100 && num < 1000){
+//         int third = num % 10;
+//         Console.WriteLine(third);
+//     }
+//     else if (num < 100)
+//         Console.WriteLine("Третьей цифры нет.");
+//     else 
+//         while (num >= 1000)
+//         num = num / 10;
+//         Console.WriteLine(num % 10);
+// }
+//  Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+//  ThirdNumber (num);
